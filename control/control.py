77b21a09a3f44fccbb65a9ab8f6181392cb1392e
huang_xinghui <772674210@qq.com>
2021-09-17 14:50:55 +0800

    update

--- control/control.py
+++ control/control.py
@@ -1,6 +1,15 @@
 import ADCPlatform
 import time
+from PIL import Image
+import numpy
+import torch
 import control.pid as pid
+from yolox.data.datasets import COCO_CLASSES
+
+import sys
+sys.path.append("../")
+
+import perception.DrivingDetection as detection
 
 # control param
 speed_kp = 1.00
@@ -12,6 +21,11 @@ speedPidThread_2 = 3000
 def init():
     speedPid.clear()
     speedPid.setSetpoint(1500)# 保持跟车15m
+
+def convert_image_to_numpy_ndarray(imageframe_byte):
+	#image2 = Image.fromarray(array) # image2 is a PIL image,array is a numpy
+	#array
+   return numpy.array(Image.open(imageframe_byte))
 
 # 启动算法  在此方法中调用实现算法控制代码
 def run():
@@ -35,6 +49,48 @@ def run():
         elif sensor.Name == "车道线传感器":
             landLineId = sensor.ID
         # print("名称：" + sensor.Name + ",ID:" + str(sensor.ID))
+    
+    # initialize network in perception
+    args = detection.make_parser().parse_args()
+    exp = detection.get_exp(args.exp_file, args.name)
+    if args.conf is not None:
+        exp.test_conf = args.conf
+    if args.nms is not None:
+        exp.nmsthre = args.nms
+    if args.tsize is not None:
+        exp.test_size = (args.tsize, args.tsize)
+    model = exp.get_model()
+    if args.device == "gpu":
+        model.cuda()
+    if args.fp16:
+        model.half()  # to FP16
+    model.eval()
+
+    if not args.trt:
+        if args.ckpt is None:
+            ckpt_file = os.path.join(file_name, "best_ckpt.pth")
+        else:
+            ckpt_file = args.ckpt
+        # logger.info("loading checkpoint")
+        ckpt = torch.load(ckpt_file, map_location="cpu")
+        # load the model state dict
+        model.load_state_dict(ckpt["model"])
+    
+    if args.trt:
+        assert not args.fuse, "TensorRT model is not support model fusing!"
+        trt_file = os.path.join(file_name, "model_trt.pth")
+        assert os.path.exists(
+            trt_file
+        ), "TensorRT model is not found!\n Run python3 tools/trt.py first!"
+        model.head.decode_in_inference = False
+        decoder = model.head.decode_outputs
+        logger.info("Using TensorRT to inference")
+    else:
+        trt_file = None
+        decoder = None
+    
+    predictor = detection.Predictor(model, exp, COCO_CLASSES, trt_file, decoder, args.device, args.fp16, args.legacy)
+    print("percetion model load.")
 
     while running:
         # 获取车辆控制数据包
@@ -46,14 +102,23 @@ def run():
         fs = control_data_package.json['FS']
         # print("当前车车速：" + str(fs))
         # 获取图片数据包 10102为摄像机类型传感器id
-        # image_package = ADCPlatform.get_image(cameraId)
+        image_package = ADCPlatform.get_image(cameraId)
+        
+        # Perception
+        img = convert_image_to_numpy_ndarray(image_package.byte)
+        # print(img.shape)
+        detection.driving_runtime(predictor, None, img, args)
+
         # 获取数据包 10101为雷达GPS等数据类型传感器id
         landLine_package = ADCPlatform.get_data(landLineId)
         # if landLine_package and len(landLine_package.json) > 0:
             # print(landLine_package.json)
         data_package = ADCPlatform.get_data(radarId)# get rradar data to follow
         # speed pid update
-        value = data_package.json[0]["Range"]
+        if data_package:
+            value = data_package.json[0]["Range"]
+        else:
+            value = None
         if(value): # for none type error
             speedPid.update(value)
             valuelast = value
