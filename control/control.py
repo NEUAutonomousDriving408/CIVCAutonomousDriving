3b718b527041e014c56fce27f66343888ec7c93c
Macmini <xunjtech@gmail.com>
2021-09-17 10:45:04 +0800

    fix speed pid control bugs

--- control/control.py
+++ control/control.py
@@ -2,8 +2,6 @@ import ADCPlatform
 import time
 import control.pid as pid
 
-# TODO:add 速度获取 结合设定速度控制车速
-
 # control param
 speed_kp = 1.20
 speed_ki = 0.02
@@ -11,6 +9,7 @@ radarPid = pid.PID(speed_kp, speed_ki, 0)
 radarPidThread_1 = 6000
 radarPidThread_2 = 3000
 
+targetSpeed = 45.0 # 想要到达的速度
 speed_kd = 0.5
 speedPid = pid.PID(speed_kp, 0, speed_kp)
 speedPidThread_1 = 10
@@ -21,33 +20,40 @@ def init():
     radarPid.clear()
 
     radarPid.setSetpoint(500.0)             # 跟车5m
-    speedPid.setSetpoint(60.0)              # 保持40km/h
+    speedPid.setSetpoint(targetSpeed)              # 保持40km/h
 
-# 速度控制 达到设定速度
+''' xld - speed pid control
+加速时能够较快达到设定目标 
+减速时能较快减到设定速度
+stage 1 - 加速
+stage 2 - 保持
+stage 3 - 微调
+stage 4 - 快速减速
+stage 5 - 减速微调
+'''
 def lontitudeControlSpeed(speed, lonPid):
-    lonPid.update(speed-6)
-    if (lonPid.output > speedPidThread_1):# 速度差太多 加速阶段
-        print('speed is:', speed, 'output is:', lonPid.output, 'stage 1')
+    lonPid.update(speed-5.0)
+    if (lonPid.output > speedPidThread_1):# 加速阶段
+        # print('speed is:', speed, 'output is:', lonPid.output, 'stage 1')
         lonPid.thorro_ = 1
         lonPid.brake_ = 0
-    elif (lonPid.output > speedPidThread_2):#2 2-10 还要加速 适当加入减速 设定值下侧
-        print('speed is:', speed, 'output is:', lonPid.output, 'stage 2')
+    elif (lonPid.output > speedPidThread_2):# 稳定控速阶段
+        # print('speed is:', speed, 'output is:', lonPid.output, 'stage 2')
         lonPid.thorro_ = min((lonPid.output / speedPidThread_1) * 0.85,1.0) #
         lonPid.brake_= min(((speedPidThread_1 - lonPid.output) / speedPidThread_1) * 0.1,1.0) #
     elif (lonPid.output > 0):# 下侧 微调
-        print('speed is:', speed, 'output is:', lonPid.output, 'stage 3')
+        # print('speed is:', speed, 'output is:', lonPid.output, 'stage 3')
         lonPid.thorro_ = (lonPid.output / speedPidThread_2) * 0.3#
         lonPid.brake_= ((speedPidThread_2 - lonPid.output) / speedPidThread_2) * 0.2
-    elif (lonPid.output < -1 * speedPidThread_2):#2 -2 - -10 减速
-        print('speed is:', speed, 'output is:', lonPid.output, 'stage 4')
-        lonPid.thorro_ = (-1 * lonPid.output / speedPidThread_2) * 0.3
-        lonPid.brake_= ((speedPidThread_2 - (-1 * lonPid.output)) / speedPidThread_2) * 0.2
-    else :
-        print('speed is:', speed, 'output is:', lonPid.output, 'stage 5')
+    elif (lonPid.output < -1 * speedPidThread_1):# 减速阶段
+        # print('speed is:', speed, 'output is:', lonPid.output, 'stage 4')
         lonPid.thorro_ = (-1 * lonPid.output / 5) * 0.2
-        lonPid.brake_= 0.4
-
-    print(lonPid.thorro_, '    ', lonPid.brake_)
+        lonPid.brake_= 0.5
+    else :
+        # print('speed is:', speed, 'output is:', lonPid.output, 'stage 5')
+        lonPid.thorro_ = (-1 * lonPid.output / speedPidThread_2) * 0.2
+        lonPid.brake_= ((speedPidThread_2 - (-1 * lonPid.output)) / speedPidThread_2) * 0.4
+    # print(lonPid.thorro_, '    ', lonPid.brake_)
 
 # radar 障碍控制 与速度有关
 def lontitudeControlRadar(value, lonPid):
@@ -109,10 +115,9 @@ def run():
         # 纵向障碍控制 speed pid update
         # radarValue = data_package.json[0]["Range"] * -1
         # lontitudeControlRadar(radarValue, radarPid)
+        # 纵向速度控制 speed pid update
         lontitudeControlSpeed(carSpeed, speedPid)
-
-        # 这里可以加入radar speed权重
-        ADCPlatform.control(speedPid.thorro_, 0, speedPid.brake_,1)
+        ADCPlatform.control(speedPid.thorro_, 0, speedPid.brake_, 1)
 
         # ADCPlatform.control(0.7, 0, 0,-1)
         # print("brake")
