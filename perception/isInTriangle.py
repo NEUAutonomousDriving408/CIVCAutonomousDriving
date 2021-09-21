6774a2348899b9eab7f2e86e62476bd9f3789450
Lordon <xunjtech@gmail.com>
2021-09-21 14:50:40 +0800

    add istriangle judgement methord

---
+++ perception/isInTriangle.py
@@ -0,0 +1,52 @@
+
+class Vector2d(object):
+    def __init__(self):
+        self.x_ = 0
+        self.y_ = 0
+    def CrossProduct(self, vector):
+        return self.x_ * vector.y_ - self.y_ * vector.x_
+
+
+    def Minus(self, vector):
+        ans = Vector2d()
+        ans.x_ = self.x_ - vector.x_
+        ans.y_ = self.y_ - vector.y_
+        return ans
+
+class Triangle(object):
+    def __init__(self):
+        self.pointA_ = Vector2d()
+        self.pointB_ = Vector2d()
+        self.pointC_ = Vector2d()
+
+    def isInTrangle(self, target):
+        PA = self.pointA_.Minus(target)
+        PB = self.pointB_.Minus(target)
+        PC = self.pointC_.Minus(target)
+
+        t1 = PA.CrossProduct(PB)
+        t2 = PB.CrossProduct(PC)
+        t3 = PC.CrossProduct(PA)
+
+        return t1 * t2 >= 0 and t1 * t3 >= 0
+
+def test():
+    triangle = Triangle()
+    triangle.pointA_.x_ = 0
+    triangle.pointA_.y_ = 0
+
+    triangle.pointB_.x_ = 6
+    triangle.pointB_.y_ = 6
+
+    triangle.pointC_.x_ = 12
+    triangle.pointC_.y_ = 0
+
+    point = Vector2d()
+    point.x_ = 5
+    point.y_ = 5.5
+
+    print(triangle.isInTrangle(point))
+
+
+if __name__ == '__main__':
+    test()
